675790384fb74689f48110dc1ba398be9744137c
kasarama <magdalena.wawrzak@gmail.com>
2020-11-27 08:40:14 +0100

    tests of SetupUsers methods  moved to  its teest class and added to git ignore

--- src/test/java/fetch/FlightFetcherTest.java
+++ src/test/java/fetch/FlightFetcherTest.java
@@ -1,18 +1,8 @@
 package fetch;
 
-import dto.AirportDTO;
-import dto.CityDTO;
-import java.io.IOException;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import utils.SetupUsers;
 
 
 /**
@@ -24,37 +14,6 @@ public class FlightFetcherTest {
     private static FlightFetcher ff = new FlightFetcher();
     
 
-    @Disabled
-    @Test
-    public void getListOfAirports() throws IOException {
-        
-        System.out.println(SetupUsers.getAirportList100(100).size());
-        for (AirportDTO a : SetupUsers.getAirportList100(100)) {
-            System.out.println(a.toString());
-        }
-
-    }
-
-    @Disabled
-    @Test
-    public void getAllAirports() throws IOException {
-
-        ArrayList<AirportDTO> list = SetupUsers.allAirports();
-        System.out.println(list.get(0).toString());
-        System.out.println(list.get(6470).toString());
-        System.out.println(list.size());
-    }
-
-   
-    
-    @Disabled
-    @Test
-    public void get100CitiesTest() throws IOException {
-        ArrayList<CityDTO> list = SetupUsers.get100Cities(0);
-        for (CityDTO ct : list) {
-            System.out.println(ct.toString());
-        }
-    }
 
     @Test
     public void dateFormatTest() throws ParseException{
--- src/test/java/fetch/SetupUsersTest.java
+++ src/test/java/fetch/SetupUsersTest.java
@@ -3,10 +3,49 @@
  */
 package fetch;
 
+import dto.AirportDTO;
+import dto.CityDTO;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import utils.SetupUsers;
+
 /**
  *
  * @author magda
  */
 public class SetupUsersTest {
     
+    @Disabled
+    @Test
+    public void getListOfAirports() throws IOException {
+        
+        System.out.println(SetupUsers.getAirportList100(100).size());
+        for (AirportDTO a : SetupUsers.getAirportList100(100)) {
+            System.out.println(a.toString());
+        }
+
+    }
+
+    @Disabled
+    @Test
+    public void getAllAirports() throws IOException {
+
+        ArrayList<AirportDTO> list = SetupUsers.allAirports();
+        System.out.println(list.get(0).toString());
+        System.out.println(list.get(6470).toString());
+        System.out.println(list.size());
+    }
+
+   
+    
+    @Disabled
+    @Test
+    public void get100CitiesTest() throws IOException {
+        ArrayList<CityDTO> list = SetupUsers.get100Cities(0);
+        for (CityDTO ct : list) {
+            System.out.println(ct.toString());
+        }
+    }
 }
