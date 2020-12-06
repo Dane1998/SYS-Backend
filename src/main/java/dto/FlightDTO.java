727b75ba354d5f55b205db0d63edef922c1ce2ee
kasarama <magdalena.wawrzak@gmail.com>
2020-12-06 01:20:15 +0100

    sorting flights list så there is not duplets

--- src/main/java/dto/FlightDTO.java
+++ src/main/java/dto/FlightDTO.java
@@ -7,7 +7,7 @@ import dto.flightFetchResult.AirlineDTO;
 import dto.flightFetchResult.ArrivalDTO;
 import dto.flightFetchResult.DepartureDTO;
 import dto.flightFetchResult.LiveDTO;
-
+import java.util.Objects;
 
 /**
  *
@@ -22,8 +22,8 @@ public class FlightDTO {
     private AirlineDTO airline;
     private LiveDTO live;
 
-    public FlightDTO( String flight_date, String flight_status, DepartureDTO departure, ArrivalDTO arrival, AirlineDTO airline, LiveDTO live) {
-        
+    public FlightDTO(String flight_date, String flight_status, DepartureDTO departure, ArrivalDTO arrival, AirlineDTO airline, LiveDTO live) {
+
         this.flight_date = flight_date;
         this.flight_status = flight_status;
         this.departure = departure;
@@ -31,7 +31,6 @@ public class FlightDTO {
         this.airline = airline;
         this.live = live;
     }
-
 
     public String getFlight_date() {
         return flight_date;
@@ -63,14 +62,43 @@ public class FlightDTO {
 
     @Override
     public String toString() {
-        return "FlightDTO{" + "flight_date=" + flight_date + ", flight_status=" + flight_status + ", departure=" + departure.getAqirport()+ ", arrival=" + arrival.getAirport()+ ", airline=" + airline.getName() + ", live=" + live.getUpdated() + '}';
+        return "FlightDTO{" + "flight_date=" + flight_date + ", flight_status=" + flight_status + ", departure=" + departure.getAqirport() + ", arrival=" + arrival.getAirport() + ", airline=" + airline.getName() + ", live=" + live.getUpdated() + '}';
     }
-    
-    
-    
-    
-    
-    
+
+    @Override
+    public int hashCode() {
+
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.flight_date);
+        hash = 29 * hash + Objects.hashCode(this.departure);
+        hash = 29 * hash + Objects.hashCode(this.arrival);
+        hash = 29 * hash + Objects.hashCode(this.airline);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final FlightDTO other = (FlightDTO) obj;
+        if (!Objects.equals(this.flight_date, other.flight_date)) {
+            return false;
+        }
+        if (!Objects.equals(this.departure.getScheduled(), other.departure.getScheduled())) {
+            return false;
+        }
+        if (!Objects.equals(this.arrival.getScheduled(), other.arrival.getScheduled())) {
+            return false;
+        }
+
+        return true;
+    }
+
 }
-
-
--- src/test/java/fetch/FlightFetcherTest.java
+++ src/test/java/fetch/FlightFetcherTest.java
@@ -1,48 +1,24 @@
 package fetch;
 
+import dto.FlightDTO;
+import dto.flightFetchResult.AirlineDTO;
+import dto.flightFetchResult.ArrivalDTO;
+import dto.flightFetchResult.DepartureDTO;
+import dto.flightFetchResult.LiveDTO;
 import java.text.ParseException;
-import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
-
 
 /**
  *
- * @author magda  and daniel
+ * @author magda and daniel
  */
 public class FlightFetcherTest {
 
     private static FlightFetcher ff = new FlightFetcher();
-    
 
+   
 
-    @Test
-    public void dateFormatTest() throws ParseException{
-    /*    String dateString="2020-11-08";
-            String[] dateArr = dateString.split("-");
-            for (int i = 0; i < dateArr.length; i++) {
-            String string = dateArr[i];
-                System.out.println(string);
-        }
-            String dateString2=dateArr[0]+"/"+dateArr[1]+"/"+dateArr[2];
-            System.out.println(dateString2);
-        Date parsedDate = new SimpleDateFormat("yyyy/MM/dd").parse(dateString2); //    Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);
-        
-System.out.println("parsed:" + parsedDate.toString());
-   String date = "2016-08-16";
-
-        //default, ISO_LOCAL_DATE
-        LocalDate localDate = LocalDate.parse(dateString);
-        LocalDate localDate1 = LocalDate.parse(date);
-         System.out.println("Date to string: "+ localDate1.toString());
-         System.out.println("Date : "+ localDate1);
-         */
-    String schStr = "2019-12-12T04:20:13+00:00";
-        // LocalDate scheduled = LocalDate.parse("2019-12-12T04:20:13+00:00");
-ZonedDateTime zonedDateTime = ZonedDateTime.parse(schStr);
-         System.out.println("TO STRING: "+zonedDateTime.toString());
-         System.out.println("hour: "+zonedDateTime.getHour());
-         System.out.println("minut: "+zonedDateTime.getMinute());
-
-    }
-    
 }
